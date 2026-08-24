eec7ab71664441302ed2c02d29249f409862ebca
nlkp-tech <nadinlatifa62@gmail.com>
2026-08-24 14:12:39 +0700

    buat route get kategori

--- jelajahpo-backend/server.js
+++ jelajahpo-backend/server.js
@@ -1,11 +1,43 @@
 const exprees = require('express');
 const app = exprees();
-const PORT = 3001;
+const mysql = require('mysql2');
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'jelajahpo_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database:', err);
+    } else {
+        console.log('Berhasil konek ke database JelajahPo');
+    }
+});
+
+const PORT = 5000;
 
 app.use(exprees.json());
 
 app.get('/', (req, res) => {
     res.send('Selamat Datang di JelajahPo API');
+});
+
+app.get('/wisata', (req, res) => {
+    const sql = 'SELECT * FROM wisata';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
 });
 
 app.listen(PORT, () => {
