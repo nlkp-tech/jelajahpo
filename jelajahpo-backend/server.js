689dc39441a8265b637a3ad9bce3f81141d93196
nlkp-tech <nadinlatifa62@gmail.com>
2026-08-24 11:32:39 +0700

    Update server.js

--- jelajahpo-backend/server.js
+++ jelajahpo-backend/server.js
@@ -1,11 +1,11 @@
 const exprees = require('express');
 const app = exprees();
-const PORT = 5000;
+const PORT = 3001;
 
 app.use(exprees.json());
 
 app.get('/', (req, res) => {
-    res.send('JelajahPo Backend API berjalan! ');
+    res.send('Selamat Datang di JelajahPo API');
 });
 
 app.listen(PORT, () => {
