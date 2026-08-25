d30d5c140829d8b13cb1c442b1311180a073aa03
nlkp-tech <nadinlatifa62@gmail.com>
2026-08-25 13:50:24 +0700

    buat akun pengguna

--- jelajahpo-backend/server.js
+++ jelajahpo-backend/server.js
@@ -89,6 +89,40 @@ app.get('/kategori', (req, res) => {
     });
 });
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+app.post ('/pengguna', async (req, res) => {
+    const { nama, email, password, no_hp } = req.body;
+
+    if (!nama || !email || !password) {
+        return res.status(400).json({ message: 'Nama, email, dan password wajib diisi' });
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const sql = 'INSERT INTO pengguna (nama, email, password, no_hp) VALUES (?, ?, ?, ?)';
+        db.query(sql, [nama, email, hashedPassword, no_hp], (err, result) => {
+
+            if (err) {
+                if (err.code === 'ER_DUP_ENTRY') {
+                    return res.status(400).json({
+                        message: 'Email sudah terdaftar, gunakan email lain'
+                    });
+                }
+
+            }
+            if (err) return res.status(500).json({ error: err.sqlMessage });
+            res.json({
+                message: 'Akun berhasil dibuat!',
+                id_pengguna: result.insertId
+            });
+        });
+    } catch (err) {
+        res.status(500).json({ error: 'Gagal mengenkripsi password' });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Server JelajahPo jalan di http://localhost:${PORT}`);
 });
